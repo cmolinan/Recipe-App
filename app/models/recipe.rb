e7bf8122d0d21994b40a9e5b73faedb8d6a3bfa2
cmolinan <cmolinan@gmail.com>
2022-12-13 13:29:16 -0500

    Add references to models and new migration files

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   has_many :recipe_foods
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
   validates :name, presence: true
   validates :description, presence: true
