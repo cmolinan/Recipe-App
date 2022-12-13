cefbe2affbb821b195e3ee1a214e63c38c919d0e
cmolinan <cmolinan@gmail.com>
2022-12-13 01:44:47 -0500

    First configuration of models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,6 @@
 class Food < ApplicationRecord
+  has_many :recipe_foods
+  belongs_to :user
+
+  validates :name, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_foods
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+
 end
